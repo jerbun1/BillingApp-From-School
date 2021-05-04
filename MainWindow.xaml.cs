c5478d9a51cde4a83731cea81b44e2f3820834a2
Jermaine henry <jerbunhenry@gmail.com>
2021-05-04 18:16:35 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿/*Date: Friday Oct 2 2020, 
+  Name: Jermaine Henry
+  Program: Creating a Object Program for Viewing */
+using BillingApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NETD3202._04_Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProjectChanged(object sender, TextChangedEventArgs e) {
+          //Data members from Program class 
+         string projectName;
+         double projectBudget;
+         double projectSpent;
+         double remainingHr;
+         string selectStatus;
+
+            //Checks to see if the Project Budget, Spent and remaining Hours are a number
+            double.TryParse(txtBudget.Text, out projectBudget);
+            double.TryParse(txtSpent.Text, out projectSpent);
+            double.TryParse(txtHoursRemain.Text, out remainingHr);
+
+
+
+
+    }
+        //Display the data of the object when clicked 
+        private void listBoxClick(object sender, RoutedEventArgs e){
+
+            Program myProgram = new Program();
+
+            //Data members of the Program class
+            string projectName = myProgram.ProjectName;
+            double projectBudget = myProgram.ProjectBudget;
+            double projectSpent = myProgram.ProjectSpent;
+            double remainingHr = myProgram.Remaininghr;
+            string selectStatus = myProgram.SelectStatus;
+
+        }
+
+        //Creates a Project in the list view on the click 
+        private void createProject_Click(object sender, RoutedEventArgs e)
+        {
+            
+            List<Program> listOfPrograms = new List<Program>();
+            
+            //creates a new Program object 
+            Program myProgram = new Program();
+
+            //Adds the Program object to the list view displaying the name
+            listBox.Items.Add(this.txtProjectName.Text);
+
+           
+
+
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿/*Date: Friday Oct 2 2020, 
+  Name: Jermaine Henry
+  Program: Creating a Object Program for Viewing */
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace BillingApp
+{
+   public class Program
+    {
+        //private Data members for the class Program 
+        private string projectName;
+        private double projectBudget;
+        private double projectSpent;
+        private double remainingHr;
+        private string selectStatus;
+        //Complete the rest mentioned in the description
+        //Constructor with parameters set to class Program 
+        public Program(string projectName, double projectBudget, double projectSpent, double remainingHr, string selectStatus) {
+            this.projectName = projectName;
+            this.projectBudget = projectBudget;
+            this.projectSpent = projectSpent;
+            this.remainingHr = remainingHr;
+            this.selectStatus = selectStatus;
+        
+        }
+
+        //Create constructor that needs to be complied and has no parameters
+        public Program()
+        {
+
+        }
+
+        //Getters and Stters for each of the private data members 
+        //Getter and Setter for Project Name 
+          public string ProjectName
+           {    
+           get {return this.projectName;}
+           set {this.projectName = value;}
+          }
+
+        //Getter and Setter for Project Budget
+        public double ProjectBudget {
+            get { return this.projectBudget; }
+            set { this.projectBudget = value; }
+        }
+
+        //Getter and Setter for Project Spent
+        public double ProjectSpent
+        {
+            get { return this.projectSpent; }
+            set { this.projectSpent = value; }
+        }
+
+        //Getter and Setter for Remaining Hours 
+        public double Remaininghr
+        {
+            get { return this.remainingHr; }
+            set { this.remainingHr = value; }
+        }
+
+        //Getter and Setter for Slected Status
+        public string SelectStatus
+        {
+            get { return this.selectStatus; }
+            set { this.selectStatus = value; }
+        }
+
+    }
+}
